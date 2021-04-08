901204fa3fa2c61c0f7b618adff1da49a1d92023
sanynikonov <nikonov.sanynikonov@gmail.com>
2021-04-08 22:33:18 +0300

    Creates color service

---
+++ PixelartCreator.Business/Colors/ColorService.cs
@@ -0,0 +1,23 @@
+﻿using PixelartCreator.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelartCreator.Business
+{
+    public class ColorService : IColorService
+    {
+        private readonly IRepository _repository;
+
+        public ColorService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Color> GetAsync(int id)
+        {
+            return await _repository.GetAsync<Color>(id);
+        }
+    }
+}
---
+++ PixelartCreator.Business/Colors/IColorService.cs
@@ -0,0 +1,13 @@
+﻿using PixelartCreator.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelartCreator.Business
+{
+    public interface IColorService
+    {
+        Task<Color> GetAsync(int id);
+    }
+}
