fce18ae83432c5cb33ecdf25044d305269200b8d
sanynikonov <nikonov.sanynikonov@gmail.com>
2021-04-11 20:19:35 +0300

    Creates PixelartOptions

---
+++ PixelartCreator.Presentation/Data/PixelartOptions.cs
@@ -0,0 +1,22 @@
+﻿using PixelartCreator.Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PixelartCreator.Presentation.Data
+{
+    public class PixelartOptions
+    {
+        [Required]
+        [Range(1, 2000)]
+        public int Height { get; set; }
+
+        [Required]
+        [Range(1, 2000)]
+        public int Width { get; set; }
+
+        public SizeUnit Unit { get; set; }
+    }
+}
