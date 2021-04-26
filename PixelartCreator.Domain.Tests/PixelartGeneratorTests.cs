1ddfffc1e410cbb870900a5e214a33d7def1a679
sanynikonov <nikonov.sanynikonov@gmail.com>
2021-04-26 22:46:58 +0300

    Extract test cases data to separate class

--- PixelartCreator.Domain.Tests/PixelartGeneratorTests.cs
+++ PixelartCreator.Domain.Tests/PixelartGeneratorTests.cs
@@ -13,46 +13,15 @@ namespace PixelartCreator.Domain.Tests
         private readonly Mock<IImageStorage> _mockStorage = new Mock<IImageStorage>();
         private readonly IPixelartGenerator _generator;
 
-        private static readonly SColor Water = SColor.FromArgb(64, 64, 255);
-        private static readonly SColor Wood = SColor.FromArgb(143, 119, 72);
-        private static readonly SColor Quartz = SColor.FromArgb(255, 252, 245);
-        private static readonly SColor Orange = SColor.FromArgb(216, 127, 51);
-        private static readonly SColor Plant = SColor.FromArgb(0, 124, 0);
-
-        private readonly SColor[] _colors =
-        {
-            Water,
-            Wood,
-            Quartz,
-            Orange,
-            Plant
-        };
-
-        private readonly SColor[,] _image =
-        {
-            { SColor.FromArgb(64, 64, 255), SColor.FromArgb(64, 65, 255), SColor.FromArgb(64, 64, 251) },
-            { SColor.FromArgb(63, 64, 255), SColor.FromArgb(62, 63, 255), SColor.FromArgb(64, 64, 240) },
-            { SColor.FromArgb(60, 64, 255), SColor.FromArgb(11, 64, 255), SColor.FromArgb(64, 64, 232) },
-        };
-
         public PixelartGeneratorTests()
         {
             _generator = new PixelartGenerator(_mockStorage.Object);
         }
 
-        [Fact]
-        public void CreatePixelart_FromImage_ReturnsAnother()
+        [Theory]
+        [MemberData(nameof(PixelartGeneratorTestsData.CreateTestData), MemberType = typeof(PixelartGeneratorTestsData))]
+        public void CreatePixelart_FromImage_ReturnsAnother(PixelizingOptions options, Image image, SColor[,] expected)
         {
-            var options = new PixelizingOptions { AvailibleColors = _colors };
-            var image = new Image { Pixels = _image };
-
-            var expected = new SColor[,]
-            {
-                { Water, Water, Water },
-                { Water, Water, Water },
-                { Water, Water, Water }
-            };
-
             var actual = _generator.CreatePixelart(image, options).Pixels;
 
             Assert.Equal(expected, actual);
---
+++ PixelartCreator.Domain.Tests/PixelartGeneratorTestsData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SColor = System.Drawing.Color;
+
+namespace PixelartCreator.Domain.Tests
+{
+    public class PixelartGeneratorTestsData
+    {
+        private static readonly SColor Water = SColor.FromArgb(64, 64, 255);
+        private static readonly SColor Wood = SColor.FromArgb(143, 119, 72);
+        private static readonly SColor Quartz = SColor.FromArgb(255, 252, 245);
+        private static readonly SColor Orange = SColor.FromArgb(216, 127, 51);
+        private static readonly SColor Plant = SColor.FromArgb(0, 124, 0);
+
+        private static readonly SColor[] _colors =
+        {
+            Water,
+            Wood,
+            Quartz,
+            Orange,
+            Plant
+        };
+
+        private static readonly SColor[,] _image =
+        {
+            { SColor.FromArgb(64, 64, 255), SColor.FromArgb(64, 65, 255), SColor.FromArgb(64, 64, 251) },
+            { SColor.FromArgb(63, 64, 255), SColor.FromArgb(62, 63, 255), SColor.FromArgb(64, 64, 240) },
+            { SColor.FromArgb(60, 64, 255), SColor.FromArgb(11, 64, 255), SColor.FromArgb(64, 64, 232) },
+        };
+
+        public static IEnumerable<object[]> CreateTestData => new List<object[]>
+        {
+            new object[]
+            {
+                new PixelizingOptions
+                {
+                    AvailibleColors = _colors
+                },
+                new Image
+                {
+                    Pixels = _image
+                },
+                new SColor[,]
+                {
+                    { Water, Water, Water },
+                    { Water, Water, Water },
+                    { Water, Water, Water }
+                }
+            }
+        };
+    }
+}
