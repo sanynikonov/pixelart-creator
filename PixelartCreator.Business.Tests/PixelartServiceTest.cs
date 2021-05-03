d34f864e96c61746d841b110d969755200fa3364
sanynikonov <nikonov.sanynikonov@gmail.com>
2021-05-03 22:31:10 +0300

    Add tests for pixelarts use cases

--- PixelartCreator.Business.Tests/PixelartServiceTest.cs
+++ PixelartCreator.Business.Tests/PixelartServiceTest.cs
@@ -4,6 +4,7 @@ using PixelartCreator.Domain;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,7 +29,7 @@ namespace PixelartCreator.Business.Tests
         }
 
         [Fact]
-        public async Task DeleteAsync_ById_DeletesPixelart()
+        public async Task DeleteAsync_ById_DeletesInRepository()
         {
             var pixelart = new Pixelart { Id = 10 };
             _repositoryMock.Setup(r => r.GetAsync<Pixelart>(pixelart.Id)).ReturnsAsync(pixelart);
@@ -82,6 +83,145 @@ namespace PixelartCreator.Business.Tests
             _generatorMock.VerifyAll();
             _repositoryMock.VerifyAll();
             _storageMock.VerifyAll();
+        }
+
+        [Fact]
+        public async Task SaveAsync_Model_SavesToRepository()
+        {
+            var model = new PixelartSaveModel
+            {
+                Description = "cool",
+                Name = "pixelart",
+                ResultPath = "path/to/result",
+                SourcePath = "path/to/source",
+                UserId = 123
+            };
+
+            _repositoryMock.Setup(r => r.AddAsync(It.IsAny<Pixelart>()));
+
+            await _service.SaveAsync(model);
+
+            _repositoryMock.Verify(r => r.AddAsync(It.Is<Pixelart>(p => PixelartAndModelAreEqual(p, model))));
+        }
+
+        [Fact]
+        public async Task UpdateAsync_Model_SavesToRepository()
+        {
+            var model = new PixelartUpdateModel
+            {
+                Id = 123,
+                Description = "cool",
+                Name = "pixelart"
+            };
+            var pixelart = new Pixelart
+            {
+                Id = 123
+            };
+
+            _repositoryMock.Setup(r => r.UpdateAsync(It.IsAny<Pixelart>()));
+            _repositoryMock.Setup(r => r.GetAsync<Pixelart>(It.IsAny<int>())).ReturnsAsync(pixelart);
+
+            await _service.UpdateAsync(model);
+
+            _repositoryMock.Verify(r => r.UpdateAsync(It.Is<Pixelart>(p => PixelartAndModelAreEqual(p, model))));
+        }
+
+        [Fact]
+        public async Task GetAsync_ById_ReturnsOneModel()
+        {
+            var pixelart = new Pixelart
+            {
+                Id = 123,
+                Name = "pixelart",
+                Description = "cool",
+                UserId = 123,
+                CreatedAt = DateTime.Now,
+                ResultPath = "path/to/result",
+                SourcePath = "path/to/source"
+            };
+
+            _repositoryMock.Setup(r => r.GetAsync<Pixelart>(It.IsAny<int>())).ReturnsAsync(pixelart);
+
+            var model = await _service.GetAsync(pixelart.Id);
+
+            _repositoryMock.Verify(r => r.UpdateAsync(It.Is<Pixelart>(p => PixelartAndModelAreEqual(p, model))));
+        }
+
+        [Fact]
+        public async Task GetAsync_ByFilter_ReturnsManyModels()
+        {
+            var pixelarts = new[]
+            {
+                new Pixelart
+                {
+                    Id = 123,
+                    Name = "pixelart",
+                    Description = "cool",
+                    UserId = 123,
+                    CreatedAt = DateTime.Now,
+                    ResultPath = "path/to/result",
+                    SourcePath = "path/to/source"
+                },
+                new Pixelart
+                {
+                    Id = 1234,
+                    Name = "pixelart1",
+                    Description = "cool1",
+                    UserId = 123,
+                    CreatedAt = DateTime.Now,
+                    ResultPath = "path/to/result",
+                    SourcePath = "path/to/source"
+                }
+            };
+            var filter = new PageFilter();
+
+            _repositoryMock.Setup(r => r.GetAsync(
+                It.IsAny<Expression<Func<Pixelart, bool>>>(), It.IsAny<int?>(), It.IsAny<int?>()))
+                .ReturnsAsync(pixelarts);
+
+            var models = await _service.GetAsync(filter);
+
+            foreach (var model in models)
+            {
+                _repositoryMock.Verify(r => r.UpdateAsync(It.Is<Pixelart>(p => PixelartAndModelAreEqual(p, model))));
+            }
+        }
+
+        private bool PixelartAndModelAreEqual(Pixelart pixelart, PixelartSaveModel model)
+        {
+            return pixelart.Name == model.Name
+                && pixelart.Description == model.Description
+                && pixelart.ResultPath == model.ResultPath
+                && pixelart.SourcePath == model.SourcePath
+                && pixelart.UserId == model.UserId;
+        }
+
+        private bool PixelartAndModelAreEqual(Pixelart pixelart, PixelartUpdateModel model)
+        {
+            return pixelart.Name == model.Name
+                && pixelart.Description == model.Description
+                && pixelart.Id == model.Id;
+        }
+
+        private bool PixelartAndModelAreEqual(Pixelart pixelart, PixelartInfoModel model)
+        {
+            return pixelart.Name == model.Name
+                && pixelart.Description == model.Description
+                && pixelart.ResultPath == model.ResultPath
+                && pixelart.SourcePath == model.SourcePath
+                && pixelart.UserId == model.UserId
+                && pixelart.Id == model.Id
+                && pixelart.CreatedAt == model.CreatedAt;
+        }
+
+        private bool PixelartAndModelAreEqual(Pixelart pixelart, PixelartListItemModel model)
+        {
+            return pixelart.Name == model.Name
+                && pixelart.Description == model.Description
+                && pixelart.ResultPath == model.ResultPath
+                && pixelart.UserId == model.UserId
+                && pixelart.Id == model.Id
+                && pixelart.CreatedAt == model.CreatedAt;
         }
     }
 }
